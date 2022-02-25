959e4195f9332a4462f39d51f74abb8896268fbb
Peter <peter.buschenreiter@student.kdg.be>
2022-02-25 11:59:52 +0100

    Getters and setters for NumberField

--- App/src/NumberField.java
+++ App/src/NumberField.java
@@ -10,4 +10,24 @@ public class NumberField {
 		this.isCrossed = false;
 		this.isDisabled = false;
 	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public boolean isCrossed() {
+		return isCrossed;
+	}
+
+	public void setCrossed() {
+		isCrossed = true;
+	}
+
+	public boolean isDisabled() {
+		return isDisabled;
+	}
+
+	public void setDisabled() {
+		isDisabled = true;
+	}
 }
