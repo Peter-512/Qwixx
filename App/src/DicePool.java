b19288710742fea2f03f0566211871f23b5e0678
Peter <peter.buschenreiter@student.kdg.be>
2022-02-25 10:18:34 +0100

    split up public and colored dice

--- App/src/DicePool.java
+++ App/src/DicePool.java
@@ -4,11 +4,16 @@ import java.util.ArrayList;
 
 public class DicePool {
 	ArrayList<Die> dicePool = new ArrayList<>();
+	Die[] publicDice = new Die[2];
 
 	public DicePool() {
-		String[] colors = { "red", "yellow", "green", "blue", "white", "white" };
+		String[] colors = { "red", "yellow", "green", "blue" };
 		for (String color : colors) {
 			dicePool.add(new Die(color));
+		}
+
+		for (int i = 0; i < 2; i++) {
+			publicDice[i] = new Die("white");
 		}
 	}
 }
