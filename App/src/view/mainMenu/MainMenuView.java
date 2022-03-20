141bb7d91190452f5d83512619713bebd5261e1b
Nova <nilufer8888@hotmail.com>
2022-03-20 22:32:13 +0100

    add title

--- App/src/view/mainMenu/MainMenuView.java
+++ App/src/view/mainMenu/MainMenuView.java
@@ -1,16 +1,22 @@
 package App.src.view.mainMenu;
 
 import javafx.geometry.Pos;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 public class MainMenuView extends BorderPane {
 	private VBox vBox;
 	private Button newGame;
 	private Button statistics;
 	private Button rules;
+	private Text qwixx;
 
 
 	public MainMenuView() {
@@ -20,19 +26,22 @@ public class MainMenuView extends BorderPane {
 
 
 	private void initializeNodes() {
+		qwixx = new Text("Qwixx");
+		qwixx.setFont(new Font(60));
 		newGame = new Button("New Game");
 		newGame.setFont(new Font(20));
 		statistics = new Button("Statistics");
 		statistics.setFont(new Font(20));
 		rules = new Button("Rules");
 		rules.setFont(new Font(20));
-		vBox = new VBox(newGame, statistics, rules);
+		vBox = new VBox(qwixx, newGame, statistics, rules);
 	}
 
 	private void layoutNodes() {
 		setCenter(vBox);
 		vBox.setAlignment(Pos.CENTER);
 		vBox.setSpacing(10);
+
 	}
 
 	Button getNewGame() {
@@ -46,4 +55,7 @@ public class MainMenuView extends BorderPane {
 	Button getRules() {
 		return rules;
 	}
+
+	Text getQwixx(){ return qwixx; }
+
 }
