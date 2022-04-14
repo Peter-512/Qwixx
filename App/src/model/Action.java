63a982a17d65625ae6462bba5fce4571efa8bdbf
Peter <peter.buschenreiter@student.kdg.be>
2022-04-14 19:44:30 +0200

    Added a field and constructors to Action

--- App/src/model/Action.java
+++ App/src/model/Action.java
@@ -4,7 +4,17 @@ public class Action {
 	int amountOfNumbersCrossed;
 	int amountOfNumbersMissed;
 	int pointsEarned;
+	boolean passedTurn;
+
+	public Action(int amountOfNumbersCrossed, int amountOfNumbersMissed, int pointsEarned) {
+		this.amountOfNumbersCrossed = amountOfNumbersCrossed;
+		this.amountOfNumbersMissed = amountOfNumbersMissed;
+		this.pointsEarned = pointsEarned;
+		passedTurn = false;
+	}
 
 	public Action() {
+		this(0, 0, 0);
+		passedTurn = true;
 	}
 }
