f7027f36e48cad3225d579373762951a84dc4cd6
nova.dockx <nova.dockx@student.kdg.be>
2022-04-15 14:52:42 +0200

    reworked database

--- Statistics/create_qwixx_db_script.sql
+++ Statistics/create_qwixx_db_script.sql
@@ -1,18 +1,22 @@
 --Username : postgres
 --Password : Student_1234
 -- CREATE DATABASE Qwixx1;
+drop DATABASE qwixx1;
+
+create DATABASE qwixx1;
 
 CREATE TABLE IF NOT EXISTS game_session
 (
     game_id               numeric PRIMARY KEY
-        CONSTRAINT NOT NULL,
+        CONSTRAINT
+            nn_game_id NOT NULL,
     start_time            time(3)
-        CONSTRAINT NOT NULL,
+        CONSTRAINT nn_starttime NOT NULL,
     end_time              time(3)
-        CONSTRAINT NOT NULL,
+        CONSTRAINT nn_endtime NOT NULL,
     amount_of_rows_locked numeric,
     duration              time
-        CONSTRAINT NOT NULL
+        CONSTRAINT nn_duration NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS player
@@ -29,7 +33,7 @@ CREATE TABLE score
 (
     points                      numeric,
     total_duration               time(3)
-        CONSTRAINT NOT NULL,
+        CONSTRAINT nn_total_time NOT NULL,
     total_amount_of_numbers_crossed numeric,
     total_score                  numeric,
     penalty_points               numeric,
@@ -60,11 +64,11 @@ CREATE TABLE IF NOT EXISTS global_statistics
     averageNumbersMissedPerTurn numeric,
     most_numbers_missed_per_turn    numeric,
     average_total_points          numeric
-        CONSTRAINT  NOT NULL,
+        CONSTRAINT nn_average_total_points NOT NULL,
     most_total_points             numeric
-        CONSTRAINT  NOT NULL,
+        CONSTRAINT nn_most_total_points NOT NULL,
     least_total_points            numeric
-        CONSTRAINT  NOT NULL,
+        CONSTRAINT nn_least_total_points NOT NULL,
     most_valuable_turn            numeric
 
 )
