7ba0f6349fafb3e4b3a0b9361740939fa6b172a8
nova.dockx <nova.dockx@student.kdg.be>
2022-04-15 17:55:41 +0200

    finalized sql script and Statistics class

--- Statistics/Statistics.java
+++ Statistics/Statistics.java
@@ -6,17 +6,11 @@ import java.sql.Statement;
 
 
 public class Statistics {
-    //TODO: gameid (gamesession), playername (nameid) (Player), score (Player), avg_turnduration, turnduration, longest/shortest turn, penalty_points (scorecard), amount penalty,
-    //TODO: most vs least value points, isCrossed
 
 
-    //GLOBAL:   avg
-    //SESSION:
 
     public static void main(String[] args) {
 
-//    }
-//    public Statistics() {
         try {
             Connection connection = DriverManager.getConnection(
                     "jdbc:postgresql://localhost:5432/qwixx1",
@@ -54,7 +48,7 @@ public class Statistics {
                     );
                                         
                                         
-                    CREATE TABLE score
+                    CREATE TABLE If NOT EXISTS score
                     (
                         points                          numeric,
                         total_duration                  time(3)
@@ -104,56 +98,6 @@ public class Statistics {
                         turn_end_time numeric,
                         turn_duration numeric
                     );""");
-//            Statement statement = connection.createStatement();
-//            statement.execute("CREATE TABLE IF NOT EXISTS game_session " +
-//                    "(gameID numeric PRIMARY KEY CONSTRAINT nn_gameID NOT NULL, " +
-//                    "startTime timestamp(3) CONSTRAINT nn_startTime NOT NULL, " +
-//                    "endTime timestamp(3) CONSTRAINT nn_endTime NOT NULL);"); // creates table
-//            statement.execute("CREATE SEQUENCE IF NOT EXISTS id_incr START 1;"); //CREATE SEQUENCE FOR game_id
-//            // statement.execute("insert into INT_leaderboard values (nextval('id_incr'),'Debil',5,date_trunc('second', now()));");
-//            //statement.execute("create sequence if not exists rank start with 1;");
-//            //statement.execute("alter sequence rank restart with 1;");
-//
-//            statement.execute("CREATE TABLE IF NOT EXISTS player(" +
-//                    "gameID numeric," +
-//                    "CONSTRAINT fk_game_ID FOREIGN KEY (gameID) REFERENCES game_session(gameID)," +
-//                    "nameID varchar(255) PRIMARY KEY," +
-//                    "score numeric CONSTRAINT nn_score NOT NULL," +
-//                    "TotalTime timestamp(3) CONSTRAINT nn_totalTime NOT NULL," +
-//                    "GameOver boolean" +
-//                    ");");
-//
-//            statement.execute("CREATE TABLE SessionStatistics(" +
-//                    "gameID numeric PRIMARY KEY," +
-//                    "averageDuration numeric," +
-//                    "averageScore numeric," +
-//                    "nameID varchar(255)," +
-//                    "averageTimeSpent numeric," +
-//                    "winPercentage numeric," +
-//                    "moveCountAverage numeric," +
-//                    "CONSTRAINT fk_game_ID FOREIGN KEY(gameID) REFERENCES game_session(gameID)," +
-//                    "CONSTRAINT fk_name_ID" +
-//                    "   FOREIGN KEY(nameID)" +
-//                    "       REFERENCES player(nameID)" +
-//                    ");");
-//
-//            statement.execute("CREATE TABLE GlobalStatistics(" +
-//                    "gameID numeric PRIMARY KEY," +
-//                    "averageTotal numeric," +
-//                    "highestTotal numeric," +
-//                    "lowestTotal numeric," +
-//                    "averageDuration numeric," +
-//                    "averageScore numeric," +
-//                    "nameID varchar(255)," +
-//                    "averageTimeSpent numeric," +
-//                    "winPercentage numeric," +
-//                    "moveCountAverage numeric," +
-//                    "CONSTRAINT fk_game_ID FOREIGN KEY(gameID) REFERENCES game_session(gameID)," +
-//                    "CONSTRAINT fk_name_ID" +
-//                    "   FOREIGN KEY(nameID)" +
-//                    "       REFERENCES player(nameID)" +
-//                    ");");
-
             connection.close();
         } catch (SQLException e) {
             System.out.println("Connection failure.");
