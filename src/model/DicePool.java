c61da2827d0fa1a549cb7d8926f3bcee503bf8b7
Peter <peter.buschenreiter@student.kdg.be>
2022-05-20 20:11:22 +0200

    DicePool implements List<Die>
    Took 4 hours 51 minutes

--- src/model/DicePool.java
+++ src/model/DicePool.java
@@ -1,8 +1,8 @@
 package src.model;
 
-import java.util.ArrayList;
+import java.util.*;
 
-public class DicePool {
+public class DicePool implements List<Die> {
 	private ArrayList<Die> dice = new ArrayList<>();
 	private final boolean isPublic;
 
@@ -40,5 +40,130 @@ public class DicePool {
 
 	public boolean isPublic() {
 		return isPublic;
+	}
+
+	@Override
+	public int size() {
+		return dice.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return dice.isEmpty();
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		return dice.contains(o);
+	}
+
+	@Override
+	public Iterator<Die> iterator() {
+		return dice.iterator();
+	}
+
+	@Override
+	public Object[] toArray() {
+		return dice.toArray();
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		return dice.toArray(a);
+	}
+
+	@Override
+	public boolean add(Die die) {
+		return dice.add(die);
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		return dice.remove(o);
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		return dice.containsAll(c);
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends Die> c) {
+		return dice.addAll(c);
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends Die> c) {
+		return dice.addAll(index, c);
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		return dice.removeAll(c);
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		return dice.retainAll(c);
+	}
+
+	@Override
+	public void clear() {
+		dice.clear();
+	}
+
+	@Override
+	public Die get(int index) {
+		return dice.get(index);
+	}
+
+	public Die get(Color color) {
+		for (Die die : getDice()) {
+			final ColoredDie colDie = (ColoredDie) die;
+			if (colDie.getColor() == color) {
+				return colDie;
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public Die set(int index, Die element) {
+		return dice.set(index, element);
+	}
+
+	@Override
+	public void add(int index, Die element) {
+		dice.add(index, element);
+	}
+
+	@Override
+	public Die remove(int index) {
+		return dice.remove(index);
+	}
+
+	@Override
+	public int indexOf(Object o) {
+		return dice.indexOf(o);
+	}
+
+	@Override
+	public int lastIndexOf(Object o) {
+		return dice.lastIndexOf(o);
+	}
+
+	@Override
+	public ListIterator<Die> listIterator() {
+		return dice.listIterator();
+	}
+
+	@Override
+	public ListIterator<Die> listIterator(int index) {
+		return dice.listIterator(index);
+	}
+
+	@Override
+	public List<Die> subList(int fromIndex, int toIndex) {
+		return dice.subList(fromIndex, toIndex);
 	}
 }
