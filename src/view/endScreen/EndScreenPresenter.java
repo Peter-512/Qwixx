53e2c92a6866d1585fc1be9e0088381bf2511fd2
Peter <peter.buschenreiter@gmail.com>
2022-05-26 12:18:25 +0200

    endScreen has a chart that shows total points over all turns
    Took 8 minutes

--- src/view/endScreen/EndScreenPresenter.java
+++ src/view/endScreen/EndScreenPresenter.java
@@ -1,13 +1,16 @@
 package src.view.endScreen;
 
-import src.Main;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Tooltip;
 import src.model.Game;
+import src.model.PlayerSession;
+import src.model.Turn;
 import src.view.mainMenu.MainMenuPresenter;
 import src.view.mainMenu.MainMenuView;
 
 public class EndScreenPresenter {
-	private Game model;
-	private EndScreenView view;
+	private final Game model;
+	private final EndScreenView view;
 
 	public EndScreenPresenter(Game model, EndScreenView view) {
 		this.model = model;
@@ -27,7 +30,25 @@ public class EndScreenPresenter {
 	}
 
 	private void updateView() {
+		for (PlayerSession playerSession : model.getGameSession().getPlayerSessions()) {
+			XYChart.Series<Number, Number> series = new XYChart.Series<>();
 
+			series.getData().add(new XYChart.Data<>(0, 0));
+			int cumPoints = 0;
+			for (Turn turn : playerSession.getTurns()) {
+				XYChart.Data<Number, Number> data = new XYChart.Data<>(turn.getTurnNumber(), cumPoints);
+				cumPoints += turn.getTotalPoints();
+				series.getData().add(data);
+			}
+			series.setName(playerSession.getPlayerName());
+			view.getChart().getData().add(series);
+
+			for (XYChart.Series<Number, Number> s : view.getChart().getData()) {
+				for (XYChart.Data<Number, Number> d : s.getData()) {
+					Tooltip.install(d.getNode(), new Tooltip(d.getYValue().toString()));
+				}
+			}
+		}
 	}
 
 
--- src/view/endScreen/EndScreenView.java
+++ src/view/endScreen/EndScreenView.java
@@ -2,6 +2,8 @@ package src.view.endScreen;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Font;
@@ -10,6 +12,7 @@ import javafx.scene.text.Text;
 public class EndScreenView extends BorderPane {
 	Text text;
 	Button button;
+	LineChart<Number, Number> chart;
 
 	public EndScreenView(boolean playerWon) {
 		initializeNodes(playerWon);
@@ -20,17 +23,34 @@ public class EndScreenView extends BorderPane {
 		text = playerWon ? new Text("You won!") : new Text("You lost!");
 		text.setFont(new Font(70));
 
+		NumberAxis xAxis = new NumberAxis();
+		xAxis.setLabel("Turns");
+		xAxis.setTickUnit(1);
+
+		NumberAxis yAxis = new NumberAxis();
+		yAxis.setLabel("Total points");
+		yAxis.setTickUnit(1);
+
+		chart = new LineChart<>(xAxis, yAxis);
+
 		button = new Button("Return to main menu");
 	}
 
 	private void layoutNodes() {
-		setCenter(text);
+		setTop(text);
+		setAlignment(text, Pos.CENTER);
+		setCenter(chart);
 		setBottom(button);
 		setAlignment(button, Pos.CENTER);
 		setMargin(button, new Insets(10));
+		button.setFocusTraversable(false);
 	}
 
 	Button getButton() {
 		return button;
+	}
+
+	public LineChart<Number, Number> getChart() {
+		return chart;
 	}
 }
