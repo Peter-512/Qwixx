cf8d3abc765dc5661e197a444ce9faeea7164d3a
Peter Buschenreiter <peter.buschenreiter@student.kdg.be>
2022-05-27 10:56:27 +0200

    removed statistics from main menu and deleted the view and presenter for them
    Took 2 minutes

--- src/view/mainMenu/MainMenuPresenter.java
+++ src/view/mainMenu/MainMenuPresenter.java
@@ -5,12 +5,10 @@ import src.view.newGame.NewGamePresenter;
 import src.view.newGame.NewGameView;
 import src.view.rules.RulesPresenter;
 import src.view.rules.RulesView;
-import src.view.statistics.StatisticsPresenter;
-import src.view.statistics.StatisticsView;
 
 public class MainMenuPresenter {
-	private Game model;
-	private MainMenuView view;
+	private final Game model;
+	private final MainMenuView view;
 
 
 	public MainMenuPresenter(Game model, MainMenuView view) {
@@ -24,7 +22,6 @@ public class MainMenuPresenter {
 	private void addEventHandlers() {
 		view.getRulesButton().setOnAction(e -> setRulesView());
 		view.getNewGameButton().setOnAction(e -> setNewGameView());
-		view.getStatisticsButton().setOnAction(e -> setStatisticsView());
 	}
 
 	private void setNewGameView() {
@@ -38,12 +35,6 @@ public class MainMenuPresenter {
 		RulesView rulesView = new RulesView();
 		RulesPresenter rulesPresenter = new RulesPresenter(model, rulesView);
 		view.getScene().setRoot(rulesView);
-	}
-
-	private void setStatisticsView() {
-		StatisticsView statisticsView = new StatisticsView();
-		StatisticsPresenter statisticsPresenter = new StatisticsPresenter(model, statisticsView);
-		view.getScene().setRoot(statisticsView);
 	}
 
 	private void updateView() {
--- src/view/mainMenu/MainMenuView.java
+++ src/view/mainMenu/MainMenuView.java
@@ -10,49 +10,35 @@ import javafx.scene.text.Text;
 public class MainMenuView extends BorderPane {
 	private VBox vBox;
 	private Button newGameButton;
-	private Button statisticsButton;
 	private Button rulesButton;
-	private Text titleText;
-
 
 	public MainMenuView() {
 		initializeNodes();
 		layoutNodes();
 	}
 
-
 	private void initializeNodes() {
-		titleText = new Text("Qwixx");
+		Text titleText = new Text("Qwixx");
 		titleText.setFont(new Font(70));
 		newGameButton = new Button("New Game");
 		newGameButton.setDefaultButton(true);
 		newGameButton.setFont(new Font(20));
-		statisticsButton = new Button("Statistics");
-		statisticsButton.setFont(new Font(20));
 		rulesButton = new Button("Rules");
 		rulesButton.setFont(new Font(20));
-		vBox = new VBox(titleText, newGameButton, statisticsButton, rulesButton);
+		vBox = new VBox(titleText, newGameButton, rulesButton);
 	}
 
 	private void layoutNodes() {
 		setCenter(vBox);
 		vBox.setAlignment(Pos.CENTER);
 		vBox.setSpacing(10);
-
 	}
 
 	Button getNewGameButton() {
 		return newGameButton;
 	}
 
-	Button getStatisticsButton() {
-		return statisticsButton;
-	}
-
 	Button getRulesButton() {
 		return rulesButton;
 	}
-
-	Text getTitleText() {return titleText;}
-
 }
